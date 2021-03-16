9131ac68aa8b62bd732187c11262244082629289
sergio-r <sergiordev@gmail.com>
2021-03-16 15:48:12 -0300

    Update data

--- src/database/datacategoria.js
+++ src/database/datacategoria.js
@@ -1,10 +1,7 @@
 const datacategoria=[
-
     'Categoria 1',
     'Categoria 2',
     'Categoria 3',
-
-
 ]
 
 export default datacategoria; 
--- src/database/navdata.js
+++ src/database/navdata.js
@@ -1,28 +1,27 @@
-const navdata=[
-    {href:"http://localhost:3000",
-        icono:'"fas fa-fw fa-folder"',
-        titulo:"Ir a la pagina web",
+const navdata = [
+    {
+        href: "http://localhost:3000",
+        icono: "fas fa-globe",
+        titulo: "Ir a la pagina web",
          
     },
-    {href:"/",
-        icono:'"fas fa-fw fa-chart-area"',
-        titulo:"Paginas",
+    {
+        href: "/",
+        icono: "fas fa-fw fa-folder",
+        titulo: "Paginas",
          
     },
-    {href:"http://localhost:3000",
-        icono:'"fas fa-fw fa-folder"',
-        titulo:"Informes",
+    {
+        href: "http://localhost:3000",
+        icono: "fas fa-fw fa-chart-area",
+        titulo: "Informes",
          
     },
-    {href:"http://localhost:3000",
-        icono:"fas fa-fw fa-table",
-        titulo:"Tablas",
-         
+    {
+        href: "http://localhost:3000",
+        icono: "fas fa-fw fa-table",
+        titulo: "Tablas",
     }
+]
 
-
-  
-
-    ]
-
-    export default navdata;
+export default navdata;
